8ed0b8000dccd4df339d8c55fbd2b8e0aa404396
Vanakoff <andrey.vanakoff@gmail.com>
2021-08-17 22:43:23 +0200

    Initial Commit

---
+++ Traffic Lights/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Traffic Lights
+//
+//  Created by Andrey Vanakoff on 17/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
