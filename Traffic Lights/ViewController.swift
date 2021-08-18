a1e17837c595395135eb665d517f733a7522f985
Vanakoff <andrey.vanakoff@gmail.com>
2021-08-18 23:56:44 +0200

    debugger removed, logic updated to correct stage

--- Traffic Lights/ViewController.swift
+++ Traffic Lights/ViewController.swift
@@ -8,12 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    @IBOutlet var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redLightView.alpha = 0.3
+        yellowLightView.alpha = 0.3
+        greenLightView.alpha = 0.3
+        
+        redLightView.layer.cornerRadius = redLightView.frame.size.width / 2
+        yellowLightView.layer.cornerRadius = yellowLightView.frame.size.width / 2
+        greenLightView.layer.cornerRadius = greenLightView.frame.size.width / 2
+        
+        startButton.layer.cornerRadius = 10
+        startButton.setTitle("Start!", for: .normal)
     }
-
-
+    
+    @IBAction func startButtonPressed() {
+        startButton.setTitle("Next", for: .normal)
+        
+        if redLightView.alpha != 1 && yellowLightView.alpha != 1 {
+            redLightView.alpha = 1
+            yellowLightView.alpha = 0.3
+            greenLightView.alpha = 0.3
+        } else if yellowLightView.alpha != 1 {
+            yellowLightView.alpha = 1
+            redLightView.alpha = 0.3
+            greenLightView.alpha = 0.3
+        } else if greenLightView.alpha != 1 {
+            greenLightView.alpha = 1
+            redLightView.alpha = 0.3
+            yellowLightView.alpha = 0.3
+        }
+    }
 }
-
